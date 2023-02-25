27ad401b0899fc2fa51894abffa6d0e1ec0257e1
Jenny Montoya <jennyjj1001@gmail.com>
2023-02-24 21:25:13 -0500

    .trim()

--- contact.js
+++ contact.js
@@ -4,7 +4,7 @@ const $mail = document.getElementById('mail');
 const handleSubmit = (e) => {
    e.preventDefault();
    const form = new FormData($form);
-   $mail.setAttribute('href',`mailto:jennyjj1001@gmail.com?subject=${form.get('name')}
+   $mail.setAttribute('href',`mailto:jennyjj1001@gmail.com?subject=${form.get('name').trim()}
    ${form.get('email')}&body=${form.get('message')}`)
    $mail.click();
 
